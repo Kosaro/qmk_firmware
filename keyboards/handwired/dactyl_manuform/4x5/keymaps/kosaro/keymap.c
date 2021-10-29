c348e2fe476b32d6cf8c40564462fdff685f17be
Kosaro <okosarkosarewicz@gmail.com>
2021-10-29 18:05:35 -0400

    pipe

--- keyboards/handwired/dactyl_manuform/4x5/keymaps/kosaro/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x5/keymaps/kosaro/keymap.c
@@ -12,17 +12,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL_T(KC_R), LALT_T(KC_S), LGUI_T(KC_N), LSFT_T(KC_T), KC_G,                                               KC_W, RSFT_T(KC_U), RGUI_T(KC_E), RALT_T(KC_I), RCTL_T(KC_A), \
         KC_X,         KC_M,         KC_L,         LT(1,KC_D),   KC_B,                                               KC_Z, LT(1,KC_F),    KC_QUOT,       KC_COMM,      KC_DOT, \
                                     LCTL(KC_C),   LCTL(KC_V),                                                                           KC_VOLD,      KC_VOLU, \
-                                                                    LT(3,KC_ENT), LT(2,KC_SPC),    LT(2,KC_ESC), LT(3,KC_BSPC), \
-                                                                    KC_TAB,      KC_BSPC,            KC_CAPS,      KC_DEL, \
-                                                                    KC_ESC, KC_DEL,                TG(4),        LT(3,KC_TAB)),
+                                                                    LT(3,KC_ENT), LT(2,KC_SPC),     LT(2,KC_ESC), LT(3,KC_BSPC), \
+                                                                    KC_TAB,       KC_BSPC,          KC_CAPS,      KC_DEL, \
+                                                                    KC_ESC,       KC_DEL,            KC_SPC,       KC_CAPS),
 	[1] = LAYOUT( \
-    KC_TILD, KC_GRV, KC_PERC, KC_NO, KC_NO, KC_CIRC, KC_DLR, KC_BSLS, KC_EXLM, KC_QUES, \
-        LCTL_T(KC_LCBR), LALT_T(KC_LPRN), LGUI_T(KC_RPRN), LSFT_T(KC_RCBR), KC_NO, KC_AMPR, RSFT_T(KC_EQL), RGUI_T(KC_UNDS), RALT_T(KC_HASH), RCTL_T(KC_COLN), \
-        KC_LT, KC_LBRC, KC_RBRC, KC_GT, KC_NO, KC_AT, KC_SLASH, KC_PLUS, KC_MINS, KC_ASTR, \
-        LCTL(KC_Z), LCTL(KC_X), KC_NO, KC_NO, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+        KC_TILD,         KC_GRV,           KC_PERC,         KC_NO,           KC_NO,                                  KC_CIRC, KC_DLR,          KC_BSLS,         KC_EXLM,         KC_PIPE, \
+        LCTL_T(KC_LCBR), LALT_T(KC_LPRN),  LGUI_T(KC_RPRN), LSFT_T(KC_RCBR), KC_NO,                                  KC_AMPR, RSFT_T(KC_EQL),  RGUI_T(KC_UNDS), RALT_T(KC_HASH), RCTL_T(KC_QUES), \
+        KC_LT,           KC_LBRC,          KC_RBRC,         KC_GT,           KC_NO,                                  KC_AT,   KC_SLASH,        KC_PLUS,         KC_MINS,         KC_ASTR, \
+                                           LCTL(KC_Z),      LCTL(KC_X),                                                       KC_NO,           KC_NO, \
+                                                                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+                                                                                KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
+                                                                                KC_TRNS, KC_TRNS, RESET,   KC_TRNS),
 	[2] = LAYOUT( \
     KC_NO, KC_BTN2, KC_MS_U, KC_BTN1, KC_NO, KC_NO, KC_PGUP, KC_UP, KC_HOME, KC_NO, \
         KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, KC_NO, KC_LEFT, KC_DOWN, KC_RGHT, KC_NO, \
@@ -34,24 +34,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[3] = LAYOUT( \
     KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, \
         KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, \
-        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_F11, KC_DOT, \
+        KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_F11, KC_F12, KC_COMM, KC_DOT, \
         KC_F11, KC_F12, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[4] = LAYOUT( \
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, \
-        KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, \
-        KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_P, KC_NO, \
-        KC_F17, KC_F18, KC_COMM, KC_DOT, \
-        MO(5), KC_SPC, KC_NO, KC_NO, \
-        KC_ENT, KC_ENT, TG(4), TG(4), \
-        KC_ESC, LSFT(KC_TAB), TG(4), TG(4)),
-	[5] = LAYOUT( \
-    KC_1, KC_2, KC_3, KC_4, KC_5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, \
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, \
-        KC_6, KC_7, KC_8, KC_9, KC_0, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, \
-        KC_VOLD, KC_VOLU, KC_NO, KC_NO, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
@@ -59,9 +43,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void keyboard_post_init_user(void) {
   // Customise these values to desired behaviour
-  debug_enable=false;
+  //debug_enable=false;
   //debug_matrix=true;
-  debug_keyboard=true;
+  //debug_keyboard=true;
   //debug_mouse=true;
 }
 
@@ -93,8 +77,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return process_key_tap(KC_UNDS, record);
         case RALT_T(KC_HASH):
             return process_key_tap(KC_HASH, record);
-        case RCTL_T(KC_COLN):
-            return process_key_tap(KC_COLN, record);
+        case RCTL_T(KC_QUES):
+            return process_key_tap(KC_QUES, record);
         default:
             return true;
     }
